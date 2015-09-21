a9540a1ff553924af4a3780e5e83ee5cf6e85bb6
zero <zero13737910852@163.com>
2015-09-21 21:34:46 +0800

    加入文档以及修复bug

--- src/ZModuleData.js
+++ src/ZModuleData.js
@@ -1,173 +1,270 @@
-/*
-	config:{
-		key:'_pageParam',//缓存管理器中的key值，默认值_pageParam
-		scope:['common','','']//缓存域，会有一个默认值common
-	}
-*/
-function ZModuleData(cacheManager,config){
-	if(!config)config={};
-	this.config=config;
-	this.cacheManager=cacheManager;
-	if(!this.config.key){
-		this.config.key='_pageParam';
-	}
-	if(!this.config.scope){
-		this.config.scope=[];
-	}
-	this.config.scope.push('common');
-	var cache={};
-	cache[this.config.key]={
-		key:this.config.key
-	};
-	this.cacheManager.addCache(cache);
-	var body=document.getElementsByTagName('body');
-	if(body){
-		body=body[0];
-	}
-	var curScope=body.getAttribute('datascope');
-	if(!curScope)curScope='common';
-	if(this._isInScope(curScope)){
-		this.curScope=curScope;
-	}
-}
-ZModuleData.prototype.use=function(scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return;}
-	this.curScope=scope;
-}
-ZModuleData.prototype.get=function(name,scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return undefined;}
-	var json=this._getJson();
-	var data=json[scope];
-	if(!data){
-		return undefined;
-	}
-	return data[name];
-}
-/*
-	两种调用方式：
-	1、md.set('mykey','myvalue','myscope');
-	2、md.set({
-		key1:val1,
-		key2:val2
-	},'myscope');
-	第二种方式会在数据域中新增传入的key
-*/
-ZModuleData.prototype.set=function(name,value,scope){
-	if(typeof name == 'object'){
-		scope=value;
-	}
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return;}
-	var json=this._getJson();
-	if(!json[scope]){
-		json[scope]={};
-	}
-	if(typeof name == 'string'){
-		json[scope][name]=value;
-	}else{
-		for(var i in name){
-			json[scope][i]=name[i];
+;(function(){
+	/*
+		config:{
+			key:'_pageParam',//缓存管理器中的key值，默认值_pageParam
+			scope:['common','','']//缓存域，会有一个默认值common
 		}
-	}
-	this.cacheManager.set(this.config.key,JSON.stringify(json));
-}
-ZModuleData.prototype.del=function(scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return;}
-	var json=this._getJson();
-	if(json&&json[scope]){
-		delete json[scope];
-		this.cacheManager.set(this.config.key,JSON.stringify(json));
-	}
-}
-ZModuleData.prototype.setTemp=function(name,value,scope){
-	if(typeof name == 'object'){
-		scope=value;
-	}
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return;}
-	var json=this._getJson();
-	if(!json[scope]){
-		json[scope]={};
-	}
-	json[scope]._temp={};
-	if(typeof name == 'string'){
-		json[scope]._temp[name]=value;
-	}else{
-		for(var i in name){
-			json[scope]._temp[i]=name[i];
+	*/
+	var ZModuleData=function(cacheManager,config){
+		if(!config)config={};
+		this.config=config;
+		this.cacheManager=cacheManager;
+		if(!this.config.key){
+			this.config.key='_pageParam';
 		}
+		if(!this.config.scope){
+			this.config.scope=[];
+		}
+		this.config.scope.push('common');
+		var cache={};
+		cache[this.config.key]={
+			key:this.config.key
+		};
+		this.cacheManager.addCache(cache);
+		this.curScope='common';
+
+		this.readyScope={};
+		this.readyTempScope={};
 	}
-	this.cacheManager.set(this.config.key,JSON.stringify(json));
-}
-ZModuleData.prototype.getTemp=function(name,scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return undefined;}
-	var json=this._getJson();
-	var data=json[scope];
-	if(data&&data._temp&&data._temp[name]){
-		return data._temp[name];
+	ZModuleData.prototype.use=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		this.curScope=scope;
 	}
-	if(data&&data[name]){
+	ZModuleData.prototype.get=function(name,scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return undefined;}
+		var data=this.getScope(scope);
+		if(!data){
+			return undefined;
+		}
 		return data[name];
 	}
-	return undefined;
-}
-ZModuleData.prototype.isTemp=function(name,scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return undefined;}
-	var json=this._getJson();
-	var data=json[scope];
-	if(data&&data._temp&&data._temp[name]){
-		return true;
-	}else{
-		return false;
+	ZModuleData.prototype.getScope=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return undefined;}
+		var json=this.readyScope[scope];
+		if(!json){
+			json=_getJson(this.cacheManager,this.config.key)[scope];
+		}
+		return json;
 	}
-}
-ZModuleData.prototype.getAllTemp=function(scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return undefined;}
-	var json=this._getJson();
-	var data=json[scope];
-	if(data&&data._temp){
-		return data._temp;
-	}else{
-		return null;
-	}
-}
-ZModuleData.prototype.commit=function(scope){
-	if(!scope)scope=this.curScope;
-	if(!this._isInScope(scope)){return;}
-	var json=this._getJson();
-	var data=json[scope];
-	if(!data||!data._temp){
-		return;
-	}
-	for(var i in data._temp){
-		data[i]=data._temp[i];
-	}
-	delete data._temp;
-	this.cacheManager.set(this.config.key,JSON.stringify(json));
-}
-/*判断当前scope是否在config中配置过*/
-ZModuleData.prototype._isInScope=function(name){
-	var flag=false;
-	var scope=this.config.scope;
-	for(var i in scope){
-		if(name == scope[i]){
-			flag=true;
+	ZModuleData.prototype.setScope=function(obj,scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=_getJson(this.cacheManager,this.config.key);
+		json[scope]=obj;
+		this.cacheManager.set(this.config.key,json);
+		if(this.readyScope[scope]){
+			this.readyScope[scope]=obj;
 		}
 	}
-	return flag;
-}
-ZModuleData.prototype._getJson=function(name){
-	var json=this.cacheManager.get(this.config.key);
-	if(!json){
-		json={};
-	}else{
-		json=JSON.parse(json);
+	/*
+		两种调用方式：
+		1、md.set('mykey','myvalue','myscope');
+		2、md.set({
+			key1:val1,
+			key2:val2
+		},'myscope');
+		第二种方式会在数据域中新增或覆盖传入的key
+	*/
+	ZModuleData.prototype.set=function(name,value,scope){
+		if(typeof name == 'object'){
+			scope=value;
+		}
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=this.getScope(scope);
+		if(!json){
+			json={};
+		}
+		if(typeof name == 'string'){
+			json[name]=value;
+		}else{
+			for(var i in name){
+				json[i]=name[i];
+			}
+		}
+		if(this.readyScope[scope]){
+			this.readyScope[scope]=json;
+		}
+		this.setScope(json,scope);
 	}
-	return json;
-}
+	ZModuleData.prototype.del=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=_getJson(this.cacheManager,this.config.key);
+		if(json&&json[scope]){
+			delete json[scope];
+			this.cacheManager.set(this.config.key,json);
+		}
+	}
+
+	ZModuleData.prototype.setTempScope=function(obj,scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=this.getScope(scope);
+		if(!json){
+			json={};
+		}
+		json._z_temp_=obj;
+		this.setScope(json,scope);
+	}
+
+	ZModuleData.prototype.getTempScope=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=this.getScope(scope);
+		if(json&&json._z_temp_){
+			for(var attr in json._z_temp_){
+				json[attr]=json._z_temp_[attr];
+			}
+		}
+		return json;
+	}
+
+
+
+	ZModuleData.prototype.setTemp=function(name,value,scope){
+		if(typeof name == 'object'){
+			scope=value;
+		}
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var json=this.getScope(scope);
+		if(!json){
+			json={};
+		}
+		json._z_temp_={};
+		if(typeof name == 'string'){
+			json._z_temp_[name]=value;
+		}else{
+			for(var i in name){
+				json._z_temp_[i]=name[i];
+			}
+		}
+		this.setScope(json,scope);
+	}
+	ZModuleData.prototype.getTemp=function(name,scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return undefined;}
+		var data=this.getScope(scope);
+		if(data&&data._z_temp_&&data._z_temp_[name]){
+			return data._z_temp_[name];
+		}
+		if(data&&data[name]){
+			return data[name];
+		}
+		return undefined;
+	}
+	ZModuleData.prototype.isTemp=function(name,scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return undefined;}
+		var data=this.getScope(scope);
+		if(data&&data._z_temp_&&data._z_temp_[name]){
+			return true;
+		}else{
+			return false;
+		}
+	}
+/*	ZModuleData.prototype.getAllTemp=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return undefined;}
+		var data=this.getScope(scope);
+		if(data&&data._z_temp_){
+			return data._z_temp_;
+		}else{
+			return null;
+		}
+	}*/
+	ZModuleData.prototype.commitTemp=function(scope){
+		if(!scope)scope=this.curScope;
+		if(!_isInScope(this.config.scope,scope)){return;}
+		var data=this.getScope(scope);
+		if(!data||!data._z_temp_){
+			return;
+		}
+		for(var i in data._z_temp_){
+			data[i]=data._z_temp_[i];
+		}
+		delete data._z_temp_;
+		this.setScope(data,scope);
+	}
+
+
+	
+
+
+	ZModuleData.prototype.ready=function(arr,fuc){
+		var data=this.readyScope;
+		for(var i in arr){
+			data[arr[i]]=this.getScope(arr[i]);
+			data[arr[i]]=data[arr[i]]?data[arr[i]]:{};
+		}
+		fuc.apply(this,[data]);
+		this.commit();
+	}
+	ZModuleData.prototype.commit=function(){
+		var scope=this.readyScope;
+		for(var attr in scope){
+			this.set(scope[attr],attr);
+		}
+	}
+
+
+	ZModuleData.prototype.on=function(id,event,clazz,callback){
+		//var data=arguments.callee.caller.arguments[0];
+		var i=(typeof arguments[2] == 'function')?2:3;
+		var cb=arguments[i];
+		if(cb){
+			arguments[i]=this.mcb(cb);
+			$(id).on(event,clazz,callback);
+		}
+	}
+
+	ZModuleData.prototype.setTimeout=function(callback,time){
+		setTimeout(this.mcb(callback),time);
+	}
+
+	ZModuleData.prototype.ajax=function(options){
+		for(var attr in options){
+			var f=options[attr];
+			if(typeof f == 'function'){
+				options[attr]=this.mcb(f);
+			}
+		}
+		$.ajax(options);
+	}
+
+	ZModuleData.prototype.mcb=function(cb){
+		var _th=this;
+		return function(){
+			var arg=[_th.readyScope];
+			for(var attr in arguments){
+				arg.push(arguments[attr])
+			}
+			cb.apply(this,arg);
+			_th.commit();
+		}
+	}
+
+	/*判断当前scope是否在config中配置过*/
+	function _isInScope(scope,name){
+		var flag=false;
+		for(var i in scope){
+			if(name == scope[i]){
+				flag=true;
+			}
+		}
+		return flag;
+	}
+	function _getJson(cacheManager,key){
+		var json=cacheManager.get(key);
+		if(!json){
+			json={};
+		}
+		return json;
+	}
+
+	window.ZModuleData=ZModuleData;
+})();
--- src/zcache.js
+++ src/zcache.js
@@ -5,7 +5,8 @@
                 key:"storeKey",
                 url:"",
                 realTime:true,
-                maxAge:0//缓存时长,毫秒,默认为0，为0的时候表示无限长
+                maxAge:0,//缓存时长,毫秒,默认为0，为0的时候表示无限长
+                count:0//缓存读取次数
             }
         },
         remoteFun:ajaxfunction
@@ -16,142 +17,251 @@
     chain.error(json);//获取数据失败
     如果不设置ajaxfunction，那么会默认使用jQuery的ajax方法来实现
 */
-
-function ZCache(config){
-    this.config=config;
-    if(!this.config.remoteFun){
-        this.config.remoteFun=function(url,param,chain){
-            $.ajax({
-                url: url,
-                type: "POST",
-                data : param +"&_=" + (new Date()).getTime(),
-                cache: false,
-                dataType: "json",
-                beforeSend: function (xhr) {
-                    xhr.overrideMimeType("text/plain; charset=utf-8");
+(function(){
+    function ZCache(config){
+        if(!config){
+            config={
+                cache:{}
+            };
+        }
+        this.config=config;
+        if(!this.config.remote){
+            this.config.remote={
+                func:function(args,tools){
+                    $.ajax({
+                        url: tools.getUrl(),
+                        type: "POST",
+                        data : args[1] +"&_=" + (new Date()).getTime(),
+                        cache: false,
+                        dataType: "json",
+                        beforeSend: function (xhr) {
+                            xhr.overrideMimeType("text/plain; charset=utf-8");
+                        },
+                        success: function(json) {
+                            tools.tag(args[1],1);
+                            tools.success(args,json);
+                        },
+                        error: function(e) {
+                            console.log(args);
+                            tools.error(args);
+                        }
+                    });
                 },
-                success: function(json) {
-                    chain.success( json );
+                success:function(args,json){
+                    args[2]('success',json);
                 },
-                error: function(e) {
-                    chain.error( "error", {"returnMessage":"服务器连接出错,请稍后再试!"} );
-                },
-                complete:function(){
+                error:function(args){
+                    args[2]('error',{returnMessage:'服务器连接出错了，请稍后再试！'});
                 }
-            });
+            }
         }
     }
-}
-ZCache.prototype.utils={
-    setStr: function(name, value) {
-        localStorage.setItem(name, value);
-    },
-    getStr: function(name) {
-        return localStorage.getItem(name);
-    },
-    remove:function(name){
-        localStorage.removeItem(name);
-    },
-    clear:function(){
-        //清除所有的存储，谨慎使用
-        localStorage.clear();
-    },
-    setJson:function(name,json){
-        var str=JSON.stringify(json);
-        this.setStr(name,str);
-    },
-    getJson:function(name){
-        var str=this.getStr(name);
-        if(str){
-            return JSON.parse(str);
-        }else{
-            return null;
-        }
-    }
-}
-ZCache.prototype.getRemote=function(name,param,callback){
-    var target=this.config.cache[name];
-    if(!target.url){
-        return;
-    }
-    var cacheKey=target.key;
-    var json=this.utils.getJson(cacheKey);
-    //可以获取缓存必须符合以下条件
-    //1、缓存中已有数据
-    //2、配置中未指定数据是实时的
-    //3、访问后端数据的入参相同
-    //4、缓存数据时长没有超过maxAge
-    var time=new Date().getTime();
-    if(json&&!target.realTime&&json._param==param&&
-        (!parseInt(target.maxAge)||(time-json._create)<parseInt(target.maxAge))){
-        callback('success',json);
-        return;
-    }else{
-        this.updateRemote(name,param,callback);
-    }
-}
-ZCache.prototype.updateRemote=function(name,param,callback){
-    var target=this.config.cache[name];
-    if(!target.url){
-        return;
-    }
-    var cacheKey=target.key;
-    var _self = this;
-    this.config.remoteFun(target.url,param,{
-        success:function(json){
-            json._param=param;
-            json._create=new Date().getTime();
-            _self.utils.setJson(cacheKey,json);
-            callback('success',json,true);
+    ZCache.prototype.utils={
+        setStr: function(name, value) {
+            localStorage.setItem(name, value);
         },
-        error:function(json){
-            _self.del(name);
-            callback('error',json,true);
+        getStr: function(name) {
+            return localStorage.getItem(name);
+        },
+        remove:function(name){
+            localStorage.removeItem(name);
+        },
+        clear:function(){
+            //清除所有的存储，谨慎使用
+            localStorage.clear();
+        },
+        setJson:function(name,json){
+            var str=JSON.stringify(json);
+            this.setStr(name,str);
+        },
+        getJson:function(name){
+            var str=this.getStr(name);
+            if(str){
+                return JSON.parse(str);
+            }else{
+                return null;
+            }
         }
-    });
-}
-ZCache.prototype.setNative=function(name,param){
-    var target=this.config.cache[name];
-    var cacheKey=target.key;
-    this.utils.setStr(cacheKey,param);
-}
-ZCache.prototype.getNative=function(name){
-    var target=this.config.cache[name];
-    var cacheKey=target.key;
-    return this.utils.getStr(cacheKey);
-}
-ZCache.prototype.get=function(name,param,callback){
-    if(callback && typeof callback == 'function'){
-        this.getRemote(name,param,callback);
-        return null;
-    }else{
-        return this.getNative(name);
     }
-}
-ZCache.prototype.set=function(name,param,callback){
-    if(callback && typeof callback == 'function'){
-        this.updateRemote(name,param,callback);
-    }else{
-        this.setNative(name,param);
+    ZCache.prototype.getRemote=function(name){
+        var target=this.config.cache[name];
+        var json=this.getNative(name);
+        //可以获取缓存必须符合以下条件
+        //1、缓存中已有数据
+        //2、配置中未指定数据是实时的
+        //3、tag字段的值相同
+        //4、缓存数据时长没有超过maxAge
+        var time=new Date().getTime();
+        if(json&&!target.realTime&&json._tagVal==arguments[json._tagIndex]&&
+            (!parseInt(target.maxAge)||(time-json._create)<parseInt(target.maxAge))&&
+            (!parseInt(target.count)||parseInt(target.count)>parseInt(json._count))){
+            json._count++;
+            this.setNative(name,json);
+            //callback('success',json._data);
+            var args=[];
+            for(var attr in arguments){
+                args.push(arguments[attr]);
+            }
+            this.config.remote.success(args,json._data);
+            return;
+        }else{
+            this.updateRemote.apply(this,arguments);
+        }
     }
-}
-ZCache.prototype.del=function(name){
-    var target=this.config.cache[name];
-    var cacheKey=target.key;
-    this.utils.remove(cacheKey);
-}
-ZCache.prototype.clearAll=function(){
-    var cache=this.config.cache;
-    for(var attr in cache){
-        var target=cache[attr];
+    
+    ZCache.prototype.updateRemote=function(name){
+        var _th = this;
+        var args=[];
+        for(var i in arguments){
+            args.push(arguments[i]);
+        }
+        this.config.remote.func.call(this,args,mkTools(this,args[0]));
+    }
+
+    function mkTools(zcache,cacheName){
+        return {
+            getUrl:function(){
+                return zcache.config.cache[cacheName].url;
+            },
+            success:function(args,data){
+                var json=zcache.getNative(cacheName);
+
+                if(!json){json={}}
+
+                json._create=new Date().getTime();
+                json._count=1;
+                json._data=data;
+                zcache.setNative(cacheName,json);
+                zcache.config.remote.success(args,data);
+            },
+            tag:function(tagVal,tagIndex){
+                var json=zcache.getNative(cacheName);
+                if(!json){json={}}
+                json._tagVal=tagVal;
+                json._tagIndex=tagIndex;
+                zcache.setNative(cacheName,json);
+            },
+            error:function(args){
+                zcache.del(cacheName);
+                zcache.config.remote.error(args);
+            }
+        }
+    }
+
+
+
+
+    ZCache.prototype.setNative=function(name,param){
+        var target=this.config.cache[name];
+        var cacheKey=target.key;
+        var json=processObj(param);
+        this.utils.setJson(cacheKey,json);
+    }
+
+    function processObj(obj){
+        if(obj instanceof Function){
+            return {
+                _z_type_:'Function',
+                _z_val_:obj.toString().replace(/\s/g,'')
+            };
+        }else if(obj instanceof Date){
+            return {
+                _z_type_:'Date',
+                _z_val_:obj.getTime()
+            };
+        }else if(obj instanceof Array){
+            var arr=[];
+            for(var i=0;i<obj.length;i++){
+               arr[i]=arguments.callee(obj[i]);
+            }
+            return arr;
+        }else if(typeof obj == 'object'){
+            var json = {};
+            for(var attr in obj){
+                json[attr] = arguments.callee(obj[attr]);
+            }
+            return json;
+        }else{
+            return obj;
+        }
+    }
+
+    ZCache.prototype.getNative=function(name){
+        var target=this.config.cache[name];
+        var cacheKey=target.key;
+        var json=this.utils.getJson(cacheKey);
+        if(!json){
+            return json;
+        }
+        json=reconsitutionObj(json);
+        return json;
+    }
+    function reconsitutionObj(obj){
+        if(!obj)return obj;
+        var type=obj._z_type_;
+        if(type == 'Function'){
+            return eval('('+obj._z_val_+')');
+        }else if(type == 'Date'){
+            return new Date(obj._z_val_);
+        }else if(obj instanceof Array){
+            var arr=[];
+            for(var i=0;i<obj.length;i++){
+               arr[i]=arguments.callee(obj[i]);
+            }
+            return arr;
+        }else if(typeof obj == 'object'){
+            var json = {};
+            for(var attr in obj){
+                json[attr] = arguments.callee(obj[attr]);
+            }
+            return json;
+        }else{
+            return obj;
+        }
+    }
+    ZCache.prototype.get=function(name){
+        if( arguments.length>1 ){
+            this.getRemote.apply(this,arguments);
+            return null;
+        }else{
+            var o=this.getNative(name);
+            if(o&&o._data){
+                return o._data;
+            }else{
+                return o;
+            }
+        }
+    }
+    ZCache.prototype.set=function(name,param){
+        var target=this.config.cache[name];
+        if(target.url){
+            var o=this.getNative(name);  
+            if(!o){o={}}
+            o._data=param;
+            this.setNative(name,o);
+        }else{
+            this.setNative(name,param);
+        }
+    }
+    ZCache.prototype.del=function(name){
+        var target=this.config.cache[name];
         var cacheKey=target.key;
         this.utils.remove(cacheKey);
     }
-}
-ZCache.prototype.addCache=function(c){
-    var cache=this.config.cache;
-    for(var i in c){
-        cache[i]=c[i];
+    ZCache.prototype.clearAll=function(){
+        var cache=this.config.cache;
+        for(var attr in cache){
+            var target=cache[attr];
+            var cacheKey=target.key;
+            this.utils.remove(cacheKey);
+        }
     }
-}
+    ZCache.prototype.addCache=function(c){
+        var cache=this.config.cache;
+        for(var i in c){
+            cache[i]=c[i];
+        }
+    }
+    window.ZCache=ZCache;
+})()
 
